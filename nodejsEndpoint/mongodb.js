9bf45b813bc2c959ea7cf39e269e70845741386c
ti391alt <Toldy12349>
2022-10-08 21:04:40 +0200

    moved project in an dev container

--- nodejsEndpoint/mongodb.js
+++ nodejsEndpoint/mongodb.js
@@ -3,8 +3,22 @@ module.exports = {
 
     connect: async function () {
         console.log("führe db call aus");
-        await mongoose.connect("mongodb://asa").then(success => console.log(success)).catch(error => console.log(error));
+        await mongoose.connect("mongodb://root:example@mongo:27017/").then(success => {
+            console.log("success");
+            console.log(success);
+        }).catch(error => {
+            console.log("error");
+            console.log(error);
+        });
         console.log("call fertig");
+        // Get the default connection
+        const db = mongoose.connection;
+        
+        
+
+        // Bind connection to error event (to get notification of connection errors)
+        db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 
     }
 }
