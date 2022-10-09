2d40e30607735c7ab2ef314f61352b10425b51ff
ti391alt <Toldy12349>
2022-10-10 00:48:54 +0200

    nodejs endpoint is ready to use

--- nodejsEndpoint/index.js
+++ nodejsEndpoint/index.js
@@ -1,14 +1,77 @@
 const express = require('express');
-
+var imgModel = require('./model');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var multer = require('multer');
+var path = require('path');
 var mongodb = require("./mongodb");
+
+
 const app = express();
 const port = 3000;
-app.get('/upload', (req, res) => {
-    res.send('upload endpoint');
-    mongodb.connect();
 
+// save received files in upload folder
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
 });
-app.get('/get', (req, res) => {
-    res.send('get endpoint');
+
+var upload = multer({ storage: storage });
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// connect to mongodb
+console.log("connecting to db: ");
+mongodb.connect();
+
+// get endpoint. all iamges are received from db and returned in response
+app.get('/getImages', (req, res) => {
+    imgModel.find({}, (err, items) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('An error occurred', err);
+        }
+        else {
+            items.forEach(element => {
+                console.log("item in images item:");
+                console.log(element);
+            });
+            res.setHeader('Content-Type', 'application/json');
+            // TODO: hier noch irgendwie alle items senden, nicht nur eins
+            res.end(JSON.stringify(items));
+            console.log("items send to requester");
+        }
+    });
+});
+
+
+// Post endpoint. Images is received here and saved into database
+app.post('/uploadImage', upload.single('image'), (req, res, next) => {
+    console.log("bin in der upload");
+
+    var obj = {
+        name: req.body.name,
+        desc: req.body.desc,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+            contentType: 'image/png'
+        }
+    }
+    console.log("object received:");
+    console.log(obj);
+    imgModel.create(obj, (err, item) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('An error occurred', err);
+        }
+        else {
+            // item.save();
+            res.status(200).send("object was saved to database sucessfully");
+        }
+    });
 });
 app.listen(port, () => console.log(`Nodejs endpoint is online and listen to port ${port}!`))
--- nodejsEndpoint/mongodb.js
+++ nodejsEndpoint/mongodb.js
@@ -1,25 +1,14 @@
 const mongoose = require('mongoose');
+var imgModel = require('./model');
 module.exports = {
 
-    connect: async function () {
-        console.log("führe db call aus");
-        await mongoose.connect("mongodb://root:example@mongo:27017/").then(success => {
-            console.log("success");
-            console.log(success);
-        }).catch(error => {
-            console.log("error");
-            console.log(error);
-        });
+    connect: function () {
+        // connect to db
+        mongoose.connect("mongodb://root:example@mongo:27017/",
+            { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+                console.log('connected')
+            });
         console.log("call fertig");
-        // Get the default connection
-        const db = mongoose.connection;
-        
-        
-
-        // Bind connection to error event (to get notification of connection errors)
-        db.on("error", console.error.bind(console, "MongoDB connection error:"));
-
-
     }
 }
 
